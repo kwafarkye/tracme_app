6eccd7e4683be45f4de90d30974030f1901ecc12
kwafarkye <kwafarkye@gmail.com>
2013-07-12 10:27:33 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TracmeApp::Application.config.secret_key_base = '2a8cb0001469e2b370238c323b161640037c74238409a9c38dbf11ea3edc638142915e9df32875af9003a8bf1a14ebe8e1b70456ad4681bfd54040944728c05f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TracmeApp::Application.config.session_store :cookie_store, key: '_tracme_app_session'
