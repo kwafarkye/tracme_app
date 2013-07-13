dd945370c14e805961322017f228e4761fbe022c
kwafarkye <kwafarkye@gmail.com>
2013-07-13 14:40:09 -0700

    Created new post and updated layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 TracmeApp::Application.routes.draw do
+  get "posts/new"
   root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/createpost', to: 'posts#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
