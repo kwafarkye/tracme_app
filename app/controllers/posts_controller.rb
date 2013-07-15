981c6f69002ca6c684a9e0b6ccecfd5a162914ed
kwafarkye <kwafarkye@gmail.com>
2013-07-14 21:11:24 -0700

    New Post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,43 @@
 class PostsController < ApplicationController
    def show
+      #@post = Post.all
       @post = Post.find(params[:id])
    end
-   
-  def new
-      @post = Post.new
-  end
+
+   def new
+      #@post = Post.new(post_params)
+   end
+
+   def create
+      @post = Post.new(post_params)
+      if @post.save
+         #Handle a successful save
+         flash[:success] = "New post created"
+         redirect_to @post
+      else
+         render 'new'
+      end
+   end
+
+   def edit
+      @post = Post.find(params[:id])
+   end
+
+   def update
+      @post = Post.find(params[:id])
+      if @post.update_attributes(post_params)
+         flash[:success] = "Post has been updated"
+         redirect_to root_url
+         #Handle a successful update
+      else
+         render 'edit'
+      end
+   end
+
+   private
+
+      def post_params
+         params.require(:post).permit(:title, :info)
+      end
+
 end
